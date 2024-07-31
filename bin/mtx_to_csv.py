2257d77045d0eb430ae8d50c008275b5065fda7c
Martin Proks <martin.proks@sund.ku.dk>
2024-07-31 13:06:48 +0200

    fix: mtx_to_csv supports gz because StarSolo returns it

--- bin/mtx_to_csv.py
+++ bin/mtx_to_csv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+from pathlib import Path
+
 import anndata
 import pandas as pd
-from pathlib import Path
 
 
 def parse_mtx(folder: str) -> anndata.AnnData:
@@ -19,15 +20,15 @@ def parse_mtx(folder: str) -> anndata.AnnData:
     anndata.AnnData
         Count matrix
     """
-    
-    adata = anndata.read_mtx(f'{folder}/matrix.mtx').T
-    adata.obs_names = pd.read_table(f'{folder}/barcodes.tsv', header=None)[0]
-    
+
+    adata = anndata.read_mtx(f"{folder}/matrix.mtx.gz").T
+    adata.obs_names = pd.read_table(f"{folder}/barcodes.tsv.gz", header=None)[0]
+
     # switch gene ids for gene symbol
-    genes = pd.read_table(f"{folder}/features.tsv", header=None, index_col=0)
-    genes.columns = ['symbol', 'other']
-    genes.index.name = 'genes'
-    adata.var = genes.reset_index().set_index('symbol')
+    genes = pd.read_table(f"{folder}/features.tsv.gz", header=None, index_col=0)
+    genes.columns = ["symbol", "other"]
+    genes.index.name = "genes"
+    adata.var = genes.reset_index().set_index("symbol")
     adata.var_names_make_unique()
 
     return adata
@@ -56,35 +57,46 @@ def main(pool: str, solo_out: str, wells: str, barcode_plate: str):
     """
 
     barcodes = pd.read_table(barcode_plate)
-    folder: str = f'{solo_out}/GeneFull/filtered/'
+    folder: str = f"{solo_out}/GeneFull/filtered"
 
     if not Path(folder).exists():
         raise IOError(f"Missing {folder}, aborting ...")
-    
+
     adata = parse_mtx(folder)
 
     # filter wells.csv based on pool
-    wells_filtered = pd.read_csv(wells).query('pool == @pool')
+    wells_filtered = pd.read_csv(wells).query("pool == @pool")
     if wells_filtered.empty:
         raise ValueError(f"After filtering for `{pool}`, no data were available ...")
-    
+
     # match barcodes based on wells
-    wells_filtered = wells_filtered.merge(barcodes, how='left', left_on='well', right='well')
-    adata.obs = adata.obs.join(wells_filtered.set_index('barcode'))
-    adata = adata[adata.obs.sort_values(by='sample').index].copy()
+    wells_filtered = wells_filtered.merge(
+        barcodes, how="left", left_on="well", right="well"
+    )
+    adata.obs = adata.obs.join(wells_filtered.set_index("barcode"))
+    adata = adata[adata.obs.sort_values(by="sample").index].copy()
 
     # save
-    adata.to_df().top_csv(f'{pool}.counts.csv')
-    
+    adata.to_df().top_csv(f"{pool}.counts.csv")
+
 
 if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser(description="Generate count matrix for sample based on well and sample name")
-    parser.add_argument('--version', action='version', version='1.0')
-    parser.add_argument('--pool', action='store', type=str, help="Pool name")
-    parser.add_argument('solo_out', action='store', type=str, help="Path to Solo.out folder")
-    parser.add_argument('wells', action='store', type=str, help="Path to wells.csv")
-    parser.add_argument('barcode_plate', action='store', type=str, help="Path to barcode set based on plate id")
+
+    parser = argparse.ArgumentParser(
+        description="Generate count matrix for sample based on well and sample name"
+    )
+    parser.add_argument("--version", action="version", version="1.0")
+    parser.add_argument("--pool", action="store", type=str, help="Pool name")
+    parser.add_argument(
+        "solo_out", action="store", type=str, help="Path to Solo.out folder"
+    )
+    parser.add_argument("wells", action="store", type=str, help="Path to wells.csv")
+    parser.add_argument(
+        "barcode_plate",
+        action="store",
+        type=str,
+        help="Path to barcode set based on plate id",
+    )
     args = parser.parse_args()
 
     main(args.pool, args.solo_out, args.wells, args.barcode_plate)
